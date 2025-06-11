691aad08e3f96631798408c746db9383da5111df
ghost <marufzonkodirov0@gmail.com>
2025-06-11 10:09:38 +0300

    daw

--- models/User.js
+++ models/User.js
@@ -60,3 +60,22 @@ userSchema.methods.getJwtToken = function () {
 };
 
 module.exports = mongoose.model('User', userSchema);
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['admin', 'doctor', 'patient'], default: 'patient' }
+});
+
+// Parolni hash qilish
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+module.exports = mongoose.model('User', userSchema);
